8ff1924320c315cf8e9539b27586f17ce8880613
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-08-14 23:17:52 -0500

    completed version 0,5

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150814203419) do
+ActiveRecord::Schema.define(version: 20150815033846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20150814203419) do
     t.integer  "login_count",     default: 0
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "admin"
   end
 
 end
