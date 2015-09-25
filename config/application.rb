e2f39a2cfc3157122ed87d7e2d994a2447ffbdb0
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-09-25 18:31:25 -0500

    ready for commit

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module ZammysGFZone
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     Koala.config.api_version = 'v2.0'
+
+
+    config.assets.version = '1.0'
+
   end
 end
